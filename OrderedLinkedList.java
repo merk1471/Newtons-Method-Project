3f3361de975480b3f981d796e2ac49021cf549ee
merk1471 <133434946+merk1471@users.noreply.github.com>
2023-10-17 17:45:09 -0400

    Update OrderedLinkedList.java

--- OrderedLinkedList.java
+++ OrderedLinkedList.java
@@ -3,43 +3,64 @@ public class OrderedLinkedList<T extends Comparable<T>> {
 	private int size;
 	
 	public OrderedLinkedList(){
-		head = null;
-		size = 0;
+		Node<T> newNode = head;
+		
 		
 	}
 	
 	public void insert(T val) {
-		Node<T> newNode = new Node<>(val, null);
-		while (head.getNext() != null && (head.getData()).compareTo(val)>0) {
-			
-			if (head.getNext() == null) {
-				break;
-			}
-			else {
-				head = head.getNext();
-			}
+		int place = 0; 
 		
-	}
-		if (head.getNext() == null) {
+		   Node<T> newNode = new Node<>(val, null);
 
-			head.setNext(newNode);
-			
-			newNode.setNext(head);
+		    if (head == null || head.getData().compareTo(val) < 0) {
+		        // Insert at the beginning of the list or into an empty list
+		        newNode.setNext(head);
+		        head = newNode;
+		    } else {
+		        Node<T> current = head;
+		        Node<T> previous = null;
+
+		        while (current != null && current.getData().compareTo(val) > 0) {
+		            previous = current;
+		            current = current.getNext();
+		        }
+
+		        // Insert between previous and current
+		        previous.setNext(newNode);
+		        newNode.setNext(current);
+		    }
+
+		    size++; // Don't forget to update the size of the list.
 		}
-		
-	
-	}
-		
-		
 	
 	
-	public void get(int i) throws IndexOutOfBoundsException {
+		
+		
+	
+	
+	public T get(int i) throws IndexOutOfBoundsException {
+		size = getSize();
 		if (i < 0 || i >= size) {
 			throw new IndexOutOfBoundsException();
 		}
-	}
+		else {
+		for (int j = 0;j<i;j++) {
+			
+			if (j != i) {
+				++j;
+				head = head.getNext();
+				
+			}
+			
+								}
+		}
+		return (head.getData());
+		}
+	
 	
 	public int getSize() {
 		return size;
 	}
 }
+
