2c5f516adba47124557519b82840ab9dac3e1a20
merk1471 <133434946+merk1471@users.noreply.github.com>
2023-10-24 18:02:24 -0400

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,33 +1,139 @@
 public class Polynomial{
-	private Monomial OrderedLinkedList;
+	private OrderedLinkedList<Monomial> Polynomial = new OrderedLinkedList<>();
 	
-	public Polynomial(){
-		//creates an empty polynomial
+	public Polynomial()
+	{
+		
 	}
 	
-	public void add(int coefficient, int degree) {
-		//adds new monomial to polynomial, monomial of same degree does not already exist in polynomial and degree is greater than or equal to 0
+	public void add(int coefficient, int degree)
+	{
+		Monomial newMonomial = new Monomial(coefficient,degree);
+		Polynomial.insert(newMonomial);
+		
 	}
 	
-	public Polynomial derivative() {
-		//returns polynomial whose derivative method was called
-		Polynomial z = new Polynomial();
-		return z;
+	public Polynomial derivative()
+	{
+		Polynomial newPolynomial = new Polynomial();
+		
+		for (int i = 0; i < Polynomial.getSize();i++) 
+		{
+			Monomial monomial = Polynomial.get(i);
+			int degree = monomial.getDegree();
+			int coefficient = monomial.getCoefficient();
+			
+			if (degree > 0)
+			{
+			int dCoefficient = coefficient * (degree);
+			int dDegree = degree - 1;
+			newPolynomial.add(dCoefficient,dDegree);
+			}
+		}
+		return  newPolynomial;
+	}
+	
+	public double eval(double x) 
+	{
+		double evaluation = 0.0;
+		
+		for (int i = 0; i < Polynomial.getSize();i++) 
+		{
+			Monomial monomial = Polynomial.get(i);
+			int degree = monomial.getDegree();
+			
+			int coefficient = monomial.getCoefficient();
+			
+			
+			double result = coefficient * Math.pow(x , degree); 
+			evaluation += result;
+		}
 
+		return evaluation;
 	}
 	
-	public double eval(double z) {
-		//returns f(z) where f is the polynomial stored in the class
-		return z;
+	public String toString() 
+	{
+		String polyAsString = "";
+		String operator = "";
+		for (int i = 0; i < Polynomial.getSize();i++)
+		{
+			Monomial monomial = Polynomial.get(i);
+			int degree = monomial.getDegree();
+			int coefficient = monomial.getCoefficient();
+			
+			if (coefficient > 0 && i != 0)
+			{
+				operator = " + ";
+			}
+			
+			else if (coefficient < 0)
+			{
+				operator = " - ";
+			}
+			
+			if (degree >= 0)
+			{
+				polyAsString += (operator + Math.abs(coefficient) + "*x^" + degree);
+			}
+		}
+
+		return (polyAsString);
 	}
 	
-	public String toString() {
-		String poly;
-		poly = "new string";
-		return poly;
-	}
-	
-	public void solve (double hello, double hi, int helloo, int hii) {
-		//newtons method
+	public double solve (double x0, double e, int T) throws SolutionNotFound
+	{
+		
+		
+		
+		//implement while loop after to check for tolerance and maxIterations
+
+		
+		
+		double current = 0.0;
+		double previous = x0;
+		
+		double atPrevious = eval(previous);
+		double atPreviousDerivative = derivative().eval(previous);
+		
+		
+		if (atPreviousDerivative != 0) 
+		{
+			current = previous - (atPrevious / atPreviousDerivative);
+		}
+		
+		else
+		{
+			throw new SolutionNotFound("divide by zero error");
+		}
+		
+		int count = 0;
+		
+		while (count < T && Math.abs(previous - current) > e)
+		{
+			previous = current;
+			atPrevious = (eval(previous));
+			atPreviousDerivative = derivative().eval(previous);
+			
+			if (atPreviousDerivative != 0) 
+			{
+			current = previous - (atPrevious / atPreviousDerivative);
+			}
+			else
+			{
+				throw new SolutionNotFound("divide by zero error");
+			}
+			 
+			++count;
+			}
+		
+		if (count >= T) 
+		{
+			throw new SolutionNotFound("maximum iteration exceeded");
+		}
+		else
+		{
+			return current; 
+		}
 	}
 }
