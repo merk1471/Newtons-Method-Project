de519ae4f5addecbe53f41e78db016f5f96095e5
merk1471 <133434946+merk1471@users.noreply.github.com>
2023-10-24 19:59:02 -0400

    Update Polynomial.java
    comments

--- Polynomial.java
+++ Polynomial.java
@@ -1,22 +1,35 @@
+/*This file contains the polynomial class with six methods: The constructor, add, eval, toString, and solve. 
+ * All of these methods are used to create, manage, interpret, and solve (using Newton's method) polynomials. 
+*/
+
 public class Polynomial{
+	//Initialize a new OrderedLinkedList named "Polynomial" to hold and sort monomials.
 	private OrderedLinkedList<Monomial> Polynomial = new OrderedLinkedList<>();
 	
+	//Constructor method, creates an empty polynomial object with no monomials. 
 	public Polynomial()
 	{
-		
+		Polynomial newPolynomial = null; 
 	}
 	
-	public void add(int coefficient, int degree)
+	//Add method, used to insert a new monomial to the OrderedLinkedList object. 
+	public void add(int coefficient, int degree) //@param, the monomials coefficient and degree. 
 	{
+		//initialize and insert a new monomial object  named "newMonomial" that holds the coefficient and degree. 
 		Monomial newMonomial = new Monomial(coefficient,degree);
 		Polynomial.insert(newMonomial);
 		
 	}
-	
+	/*Derivative method, used to obtain a new polynomial containing the derivative of the polynomial 
+	 * stored as monomials in the OrderedLinkedList object. 
+	*/
 	public Polynomial derivative()
 	{
 		Polynomial newPolynomial = new Polynomial();
 		
+		/*for loop that takes the coefficient and degree contained in each monomial, creates a new monomial containing
+		 * the updated derivative values, and adds it's derivative to the new polynomial object. 
+		 */
 		for (int i = 0; i < Polynomial.getSize();i++) 
 		{
 			Monomial monomial = Polynomial.get(i);
@@ -30,13 +43,17 @@ public class Polynomial{
 			newPolynomial.add(dCoefficient,dDegree);
 			}
 		}
-		return  newPolynomial;
+		return  newPolynomial; //@return the new polynomial object containing the derivative. 
 	}
 	
-	public double eval(double x) 
+	//eval method, used to evaluate the polynomial at a given double value. 
+	public double eval(double x) //@param, double value which the polynomial will be evaluated at
 	{
 		double evaluation = 0.0;
 		
+		/* for loop that takes the coefficient and degree in each monomial, evaluates it with the 
+		 * given value, and adds it to the double "evaluation".
+		*/
 		for (int i = 0; i < Polynomial.getSize();i++) 
 		{
 			Monomial monomial = Polynomial.get(i);
@@ -49,13 +66,16 @@ public class Polynomial{
 			evaluation += result;
 		}
 
-		return evaluation;
+		return evaluation; //@return, new double obtained after evaluating polynomial. 
 	}
 	
+	//toString method, used to return the polynomial as a string
 	public String toString() 
 	{
 		String polyAsString = "";
 		String operator = "";
+		
+		//for loop that gets the coefficient and degree from each monomial, adds the correct operator, and adds it to a string
 		for (int i = 0; i < Polynomial.getSize();i++)
 		{
 			Monomial monomial = Polynomial.get(i);
@@ -78,30 +98,27 @@ public class Polynomial{
 			}
 		}
 
-		return (polyAsString);
+		return (polyAsString); //@return, new string describing the polynomial 
 	}
 	
-	public double solve (double x0, double e, int T) throws SolutionNotFound
+	//solve method, used to find the solution of the polynomial using Newton's method. 
+	public double solve (double x0, double e, int T) throws SolutionNotFound //@param, initial value, stop value, and max attempts.
 	{
-		
-		
-		
-		//implement while loop after to check for tolerance and maxIterations
-
-		
-		
 		double current = 0.0;
 		double previous = x0;
 		
+		//values for evaluation of previous at the polynomial and it's derivative. using derivative and eval methods. 
 		double atPrevious = eval(previous);
 		double atPreviousDerivative = derivative().eval(previous);
 		
 		
+		//if the previous value at the derivative is not 0, using Newton's method, update the current value.
 		if (atPreviousDerivative != 0) 
 		{
 			current = previous - (atPrevious / atPreviousDerivative);
 		}
 		
+		//throw SolutionNotFound exception as the divisor (previous at derivative) will be 0
 		else
 		{
 			throw new SolutionNotFound("divide by zero error");
@@ -109,6 +126,7 @@ public class Polynomial{
 		
 		int count = 0;
 		
+		//while the *
 		while (count < T && Math.abs(previous - current) > e)
 		{
 			previous = current;
@@ -119,21 +137,23 @@ public class Polynomial{
 			{
 			current = previous - (atPrevious / atPreviousDerivative);
 			}
+			
 			else
 			{
 				throw new SolutionNotFound("divide by zero error");
 			}
 			 
 			++count;
-			}
+		}
 		
 		if (count >= T) 
 		{
 			throw new SolutionNotFound("maximum iteration exceeded");
 		}
+		
 		else
 		{
-			return current; 
+			return current; //@return the value of the solution
 		}
 	}
 }
